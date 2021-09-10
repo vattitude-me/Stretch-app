5c271a4ac8d58d11726868dd7f61a1aeebd5a070
Srivatsan Sridhar <vatsakrish@gmail.com>
2021-09-10 17:50:43 -0400

    major alert functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,94 +1,17 @@
 /*!
- * Start Bootstrap - Creative v7.0.4 (https://startbootstrap.com/theme/creative)
- * Copyright 2013-2021 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ * Stretch App - V1.0 (Developed by https://www.vattitude.me)
+ * Copyright 2021 Stretch App - Vattitude
+ * Licensed under MIT (https://github.com/vatsakrish/Stretch-app/blob/main/LICENSE)
  */
 //
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function() {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function(responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-
-});
-
-
-// Code to handle the timer component
-var defaultimer = 30;
-var defaulNotif = "Stand-up and shake a bit"
-
-function clockUpdate() {
-    var date = new Date();
-
-
-    function addZero(x) {
-        if (x < 10) {
-            return x = '0' + x;
-        } else {
-            return x;
-        }
-    }
-
-    function twelveHour(x) {
-        if (x > 12) {
-            return x = x - 12;
-        } else if (x == 0) {
-            return x = 12;
-        } else {
-            return x;
-        }
-    }
-
-    var h = addZero(twelveHour(date.getHours()));
-    var m = addZero(date.getMinutes());
-    var s = addZero(date.getSeconds());
-
-    $('.digital-clock').text(h + ':' + m + ':' + s)
-}
-
-
+// Global variables
+var defaultimer = 3;
+var defaulNotif = "Stand-up and shake a bit";
+var nIntervId;
 
 $(document).ready(function() {
 
@@ -100,15 +23,59 @@ $(document).ready(function() {
     $("#timerupdate").val(defaultimer);
     $("#Notiftext").val(defaulNotif);
 
-    //main notification feature
+    //Check notification feature
     checkNotify();
 
+    //Test Notification
     $('#testNotif').click(function() {
         notifyMe();
     });
 
+    //Start the main notification
+    startNotif()
+
+    //Update the Feq 
+    $('#savefreq').click(function() {
+        defaultimer = $('#timerupdate').val();
+        $("#nextrem").text(defaultimer);
+        startNotif();
+    });
+
+    $('#togglereminder').click(function() {
+        tglRem = $("#togglereminder").text();
+        if (tglRem == "Stop Reminder") {
+            stopNotif();
+        } else {
+            startNotif();
+        }
+
+    });
+
 });
 
+//Main notification
+function startNotif() {
+    $(".running").show();
+    $(".stopped").hide();
+    $("#togglereminder").text("Stop Reminder");
+
+    intervalLoop = defaultimer * 60 * 1000
+    $("#nextrem").text(defaultimer);
+    clearInterval(nIntervId);
+    nIntervId = setInterval(beginStrEvent, intervalLoop);
+}
+
+function stopNotif() {
+    clearInterval(nIntervId);
+    $(".running").hide();
+    $(".stopped").show();
+    $("#togglereminder").text("Start Reminder");
+}
+
+function beginStrEvent() {
+    console.log("Event beginStrEvent triggered")
+    notifyMe();
+}
 
 function notifyMe() {
     if (Notification.permission === "granted") {
@@ -169,3 +136,87 @@ function checkNotify() {
         }
     });
 })(jQuery);
+
+
+/*!
+ * Start Bootstrap - Creative v7.0.4 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function() {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function(responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+});
+
+
+
+function clockUpdate() {
+    var date = new Date();
+
+
+    function addZero(x) {
+        if (x < 10) {
+            return x = '0' + x;
+        } else {
+            return x;
+        }
+    }
+
+    function twelveHour(x) {
+        if (x > 12) {
+            return x = x - 12;
+        } else if (x == 0) {
+            return x = 12;
+        } else {
+            return x;
+        }
+    }
+
+    var h = addZero(twelveHour(date.getHours()));
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+
+    $('.digital-clock').text(h + ':' + m + ':' + s)
+}
