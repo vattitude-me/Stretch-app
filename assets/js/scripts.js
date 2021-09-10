90f4e8c16787f0bb4f8d989e4730719aaa22c2dd
Srivatsan Sridhar <vatsakrish@gmail.com>
2021-09-10 10:56:32 -0400

    Redesign UI

---
+++ assets/js/scripts.js
@@ -0,0 +1,100 @@
+/*!
+ * Start Bootstrap - Creative v7.0.4 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function() {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function(responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+});
+
+
+// Code to handle the timer component
+var defaultimer = 30;
+
+function clockUpdate() {
+    var date = new Date();
+
+
+    function addZero(x) {
+        if (x < 10) {
+            return x = '0' + x;
+        } else {
+            return x;
+        }
+    }
+
+    function twelveHour(x) {
+        if (x > 12) {
+            return x = x - 12;
+        } else if (x == 0) {
+            return x = 12;
+        } else {
+            return x;
+        }
+    }
+
+    var h = addZero(twelveHour(date.getHours()));
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+
+    $('.digital-clock').text(h + ':' + m + ':' + s)
+}
+
+
+
+$(document).ready(function() {
+
+    //Start the clock and keep incrementing
+    clockUpdate();
+    setInterval(clockUpdate, 999);
+
+    $("#timerupdate").val(defaultimer);
+
+});
